1cb1fbaa9d1df487cbfd5952be1bb0f013eafbfe
cchen <cchen@masdar.ac.ae>
2015-04-17 21:15:01 +0400

    get data from server

--- client/www/js/historyview.js
+++ client/www/js/historyview.js
@@ -74,8 +74,8 @@ function setd3(){
                     }).trigger("resize");
                     // Get the data
                     var node_id = getParameterByName('id');
-                    // d3.json("http://ec2-54-148-238-83.us-west-2.compute.amazonaws.com/api/getdata", function(error, data) {
-                    d3.json("http://127.0.0.1:8000/getdata/"+node_id, function(error, data) {
+                    d3.json("http://ec2-54-148-238-83.us-west-2.compute.amazonaws.com/api/getdata"+node_id, function(error, data) {
+                    // d3.json("http://127.0.0.1:8000/getdata/"+node_id, function(error, data) {
 
                         if (error)
                             return console.error(error);
